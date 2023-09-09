79a2f8ea1bc8585af75b00a31817bd15ccaf31a2
farizajju0804 <ajfariz13@gmail.com>
2023-09-09 17:11:39 +0530

    schema change header

--- src/models/ArticleComponents/ArticleHeader/ArticleHeader.js
+++ src/models/ArticleComponents/ArticleHeader/ArticleHeader.js
@@ -16,21 +16,60 @@ const ArticleHeader = ({
   const introBoxStyle = {
     backgroundImage: `url(${introMainImage})`, // Set the background image dynamically
   };
-  const processIntroContent = (content) => {
-    if (typeof content === "string") {
-      const formattedContent = content.replace(/<sup><link>(.*?)<\/link><\/sup>/g, (_, link) => {
-        const linkParts = link.split("|");
-        if (linkParts.length === 2) {
-          const [number, url] = linkParts;
-          return `<sup><a class="superscript-link" href="${url}" target="_blank" rel="noopener noreferrer">${number}</a></sup>`;
+  const processContent = (content) => {
+    if (Array.isArray(content)) {
+      return content.map((sentence, index) => {
+        if (typeof sentence === "string") {
+          let formattedSentence = sentence.replace(
+            /<strong>(.*?)<\/strong>/g,
+            '<strong>$1</strong>'
+          );
+          formattedSentence = formattedSentence.replace(
+            /<italic>(.*?)<\/italic>/g,
+            '<em class="italic">$1</em>'
+          );
+          formattedSentence = formattedSentence.replace(
+            /<sup><link>(.*?)<\/link><\/sup>/g,
+            (_, link) => {
+              const linkParts = link.split("|");
+              if (linkParts.length === 2) {
+                const [number, url] = linkParts;
+                return `<sup><a class="superscript-link" href="${url}" target="_blank" rel="noopener noreferrer">${number}</a></sup>`;
+              }
+              return "";
+            }
+          );
+          formattedSentence = formattedSentence.replace(
+            /<link>(.*?)<\/link>/g,
+            (_, link) => {
+              const linkParts = link.split("|");
+              if (linkParts.length === 2) {
+                const [text, url] = linkParts;
+                return `<a class="link" href="${url}" target="_blank" rel="noopener noreferrer">${text}</a>`;
+              }
+              return "";
+            }
+          );
+          // Handle <br> tags
+          formattedSentence = formattedSentence.replace(
+            /<br>/g,
+            '<br />'
+          );
+          return (
+            <span
+              key={index}
+              dangerouslySetInnerHTML={{ __html: formattedSentence }}
+            />
+          );
+        } else {
+          return <span key={index}>{sentence}</span>;
         }
-        return "";
       });
-      return <span dangerouslySetInnerHTML={{ __html: formattedContent }} />;
     } else {
-      return content;
+      return <span>{content}</span>;
     }
   };
+
   return (
     <div className="article-header" style={bgStyle}>
       <div className="article-title">
@@ -53,7 +92,7 @@ const ArticleHeader = ({
           </div>
           <div className="intro-content">
             <span>
-              <span>{processIntroContent(introContentNormal)}</span>
+              <span>{processContent(introContentNormal)}</span>
             </span>
           </div>
         </div>
