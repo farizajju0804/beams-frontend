497bf2e48e7a927e98f32538b5976990a922f71b
farizajju0804 <ajfariz13@gmail.com>
2023-09-10 17:11:34 +0530

    image optimize

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import LandingPage from "./pages/Landing Page/LandingPage";
 import ScrollToTop from "./ScrollToTop";
 import "./App.css"
 import { CookiesProvider } from "react-cookie";
-import ReactGA from "react-ga4";
+
 // sample comment
 function App() {
-	ReactGA.initialize("G-KT4TNN89SS");
+
 	return (
 		<CookiesProvider>
 
