da86e78a46c1fde8b0353ee2ff3dd8b837ca61ba
Ajmal Fariz <ajfariz13@gmail.com>
2023-10-29 16:20:42 +0530

    wonderland added

--- src/pages/View-Doyouknow/Viewdoyouknow.js
+++ src/pages/View-Doyouknow/Viewdoyouknow.js
@@ -11,18 +11,18 @@ function Viewdoyouknow() {
   ];
   return (
     <div>
-      <iframe
+      {/* <iframe
         loading="lazy"
         className='canva-embed'
         src={url} // Use the URL from the prop
         scrolling="yes" frameBorder="0" allowTransparency="true" allowFullScreen="true"
           >
       </iframe>
-      {/* <div style={{position: 'relative', paddingTop: 'max(60%,324px)', width: '100%', height: '0'}}>
+      <div style={{position: 'relative', paddingTop: 'max(60%,324px)', width: '100%', height: '0'}}>
         <iframe style={{position: 'absolute', border: 'none', width: '100%', height: '80%', marginTop:"10vh", left: '0', top: '0'}} src="https://online.fliphtml5.com/vryvr/rqil/" scrolling="no" frameBorder="0" allowTransparency="true" allowFullScreen="true"></iframe>
       </div> */}
-      {/* <DocViewer documents={docs} /> */}
-    </div>
+      <iframe title="Magical Materials" className="no-margin canva-embed" src="https://beams.outgrow.us/653e1e2fd8432a770d10daae" allow="camera *;"></iframe>
+     </div>
   );
 }
 
