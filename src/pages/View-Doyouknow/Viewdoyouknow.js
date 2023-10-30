3538426f8f9009e863e9e37a69ab4d48263df99d
Ajmal Fariz <ajfariz13@gmail.com>
2023-10-30 13:45:13 +0530

    wonderland quesstion

--- src/pages/View-Doyouknow/Viewdoyouknow.js
+++ src/pages/View-Doyouknow/Viewdoyouknow.js
@@ -21,8 +21,9 @@ function Viewdoyouknow() {
       <div style={{position: 'relative', paddingTop: 'max(60%,324px)', width: '100%', height: '0'}}>
         <iframe style={{position: 'absolute', border: 'none', width: '100%', height: '80%', marginTop:"10vh", left: '0', top: '0'}} src="https://online.fliphtml5.com/vryvr/rqil/" scrolling="no" frameBorder="0" allowTransparency="true" allowFullScreen="true"></iframe>
       </div> */}
-      <iframe title="Magical Materials" className="no-margin canva-embed" src={url}
-       allow="camera *;"></iframe>
+      {/* <iframe title="Magical Materials" className="no-margin canva-embed" src={url}
+       allow="camera *;"></iframe> */}
+       <iframe className='canva-embed' src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fa4349vha7EZ1Dt7WESZ8St%2FBeams-Wonderland%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-20%26viewport%3D379%252C235%252C0.17%26t%3DJqei4vIb80CC7f1p-1%26scaling%3Dscale-down%26starting-point-node-id%3D1%253A20%26mode%3Ddesign" allowfullscreen></iframe>
      </div>
   );
 }
