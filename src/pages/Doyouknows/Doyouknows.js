c6ff339a49e2a1547b213eea94506bf5f5f68c0e
Ajmal Fariz <ajfariz13@gmail.com>
2023-11-08 17:22:39 +0530

    protopie link

--- src/pages/Doyouknows/Doyouknows.js
+++ src/pages/Doyouknows/Doyouknows.js
@@ -11,9 +11,7 @@ function Doyouknows() {
       categoryColor: "#026AA2",
       categoryBgColor: "#F0F9FF",
       img: "Assets/images/thumbnail-wonder-1.webp",  
-      slideshowUrl: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fa4349vha7EZ1Dt7WESZ8St%2FBeams-Wonderland%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-20%26viewport%3D379%252C235%252C0.17%26t%3DJqei4vIb80CC7f1p-1%26scaling%3Dscale-down%26starting-point-node-id%3D1%253A20%26mode%3Ddesign"
-      // slideshowUrl: "https://quizizz.com/embed/quiz/6542717158e3e7d78f99ec8f"
-      
+      slideshowUrl: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fa4349vha7EZ1Dt7WESZ8St%2FBeams-Wonderland%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-20%26viewport%3D379%252C235%252C0.17%26t%3DJqei4vIb80CC7f1p-1%26scaling%3Dscale-down%26starting-point-node-id%3D1%253A20%26mode%3Ddesign"  
     },
     {
         category: "Magical Materials",
@@ -22,7 +20,7 @@ function Doyouknows() {
         categoryBgColor: "#FFF6F5",
         img: "Assets/images/thumbnail-wonder-2-.webp",
         // slideshowUrl: "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fa4349vha7EZ1Dt7WESZ8St%2FBeams-Wonderland%3Fpage-id%3D14%253A193%26type%3Ddesign%26node-id%3D21-203%26viewport%3D938%252C225%252C0.09%26t%3DvBW1sOxTgbwGJPmZ-1%26scaling%3Dcontain%26starting-point-node-id%3D21%253A194%26mode%3Ddesign"
-        slideshowUrl: "https://quizizz.com/embed/quiz/6542717158e3e7d78f99ec8f"
+        slideshowUrl: "https://cloud.protopie.io/p/4b05aaa8638ed4749929de41?ui=true&scaleToFit=true&enableHotspotHints=true&cursorType=touch&mockup=true&bgColor=%23F5F5F5&playSpeed=1"
       },
     
   ];
